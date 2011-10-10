9365ae1c476ca25a201c5227122c34aca80984c9
Thomas Sonntag <git@sonntagsbox.de>
2011-10-10 15:12:59 +0200

    added decorator

---
+++ lib/tracks_grid/decorator.rb
@@ -0,0 +1,33 @@
+module TracksGrid
+
+  class Decorator
+    attr_reader :model, :view_context
+
+    class << self
+      def create( model, view_context )
+        decorator_class(model).new model, view_context 
+      end
+
+      private
+      def decorator_class( model )
+        clazz = model.class.name + 'Decorator'
+        clazz.constantize rescue self
+      end
+
+    end
+
+    attr_reader :view_context, :model
+
+    def initialize( model, view_context )
+      @model, @view_context = model, view_context
+    end
+
+    def method_missing( *args )
+      @model.send *args
+    end
+
+    alias_method :helpers, :view_context 
+    alias_method :h, :view_context 
+  end
+
+end
