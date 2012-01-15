295586021e3149d21467939cce70e19bb98fc822
Thomas Sonntag <git@sonntagsbox.de>
2012-01-15 11:10:28 +0100

    added reader_breadcrumbs

--- lib/tracks_grid/breadcrumbs.rb
+++ lib/tracks_grid/breadcrumbs.rb
@@ -3,20 +3,30 @@ require 'active_support/concern'
 module TracksGrid
   module Breadcrumbs
     extend ActiveSupport::Concern
-  
-    def breadcrumbs( join = '>' )
+
+    def breadcrumbs
       @breadcrumbs ||= begin
         p = {}
         text = @filters.map do |filter, value|
-          s =  h.content_tag :span, "#{filter.label} : ", :class => 'search_key'
-          s += h.content_tag :span, value,                :class => 'search_value'
+          p[filter.label] = value
+        end
+        p
+      end
+    end
+
+    def render_breadcrumbs( join = '>' )
+      @rendered_breadcrumbs ||= begin
+        p = {}
+        text = breadcrumbs.map do |label, value|
+          s =  h.content_tag :span, "#{label} : ", :class => 'breadcrumb_key'
+          s += h.content_tag :span, value,         :class => 'breadcrumb_value'            
           p[filter.name] = value
           h.link_to s, url_for(p)
         end.join(join)
         h.content_tag :span, text, {:class => 'breadcrumbs'}, false
       end
     end
- 
+
   end
- 
+
 end
