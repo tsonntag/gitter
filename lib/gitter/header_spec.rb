cb137784cf4395f01eb8e9443659f7f7d80dfc3d
Thomas Sonntag <git@sonntagsbox.de>
2012-02-09 17:42:38 +0100

    rowspan for header

--- lib/gitter/header_spec.rb
+++ lib/gitter/header_spec.rb
@@ -6,16 +6,17 @@ module Gitter
       new :blank, false
     end
 
-    attr_reader :name, :content, :span, :column_spec
+    attr_reader :name, :content, :colspan, :rowspan, :column_spec
 
     def initialize name, content, opts = {}
       @name, @content = name, content
-      @span = opts[:span] || 1
+      @colspan = opts[:colspan] || 1
+      @rowspan = opts[:rowspan] || 1
       @column_spec = opts[:column_spec]
     end
 
     def to_s
-      "HeaderSpec(#{name})#{span > 1 ? ",#{span}": ""}#{column_spec ? ',col' : ''})"
+      "HeaderSpec(#{name},colspan=#{colspan},rowspan=#{rowspan}#{column_spec ? ',col' : ''})"
     end
   end
 
