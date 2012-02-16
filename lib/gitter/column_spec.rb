44232d9649346914932810e39d99c3215c3b5927
Thomas Sonntag <git@sonntagsbox.de>
2012-02-16 10:47:27 +0100

    added defautl header

--- lib/gitter/column_spec.rb
+++ lib/gitter/column_spec.rb
@@ -6,17 +6,20 @@ module Gitter
 
     def initialize( name, opts = {}, &block )
       @name = name
-      @header_specs = [opts[:header] || opts[:headers]].flatten.compact.map do |header_spec|
-	       puts "AAAAAAAAAAAAA #{header_spec.inspect}"
-        case header_spec
-	when Hash
-          content = header_spec.delete(:content)
-	  opts = header_spec
-	else
-          content = header_spec
-	  opts = {}
+      if header_opts = opts[:header] || opts[:headers]
+         @header_specs = [header_opts].flatten.map do |header_spec|
+           case header_spec
+           when Hash
+             content = header_spec.delete(:content)
+             opts = header_spec
+           else
+             content = header_spec
+             opts = {}
+            end
+          HeaderSpec.new name, content, opts.merge(:column_spec => self)
         end
-        HeaderSpec.new name, content, opts.merge(:column_spec => self)
+      else
+        @header_specs = [HeaderSpec.new(name, nil, opts.merge(:column_spec => self))]
       end
       @attr = opts[:column] || name
       @order = case opts[:order] 
